73a5085039e1068c3fb8abca229c33b0cb4ff8e5
Mortimer Cold <mortimer.cold@gmail.com>
2019-05-01 16:17:45 +0200

    implemented category retrieval and display

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialImports } from './material-imports';
+import { CategoryOverviewComponent } from './components/category-overview/category-overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryOverviewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+      ReactiveFormsModule,
+      HttpClientModule,
+      MaterialImports
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/services/categories.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { AppSettings } from '../settings/app-settings';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+
+constructor(private httpClient: HttpClient) { }
+
+    public getCategories() {
+        let httpOptions = {
+          headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+          })
+        };
+
+        return this.httpClient.get(AppSettings.API_ENDPOINT + 'categories', httpOptions);
+    }
+
+}
