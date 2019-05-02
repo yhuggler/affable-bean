c748916c3e140a4a07df51675bba541c5b344050
Mortimer Cold <mortimer.cold@gmail.com>
2019-05-02 10:50:38 +0200

    minor change

--- client/src/app/routes/categories/categories.component.ts
+++ client/src/app/routes/categories/categories.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, ChangeDetectorRef, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatSidenav } from '@angular/material';
 import { ProductsService } from '../../services/products.service';
-import {MediaMatcher} from '@angular/cdk/layout';
 import { switchMap } from 'rxjs/operators';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
@@ -14,24 +13,13 @@ export class CategoriesComponent implements OnInit {
 
     public products: Object[];
     public activeCategory: number;
-    public mobileQuery: MediaQueryList;
-    private _mobileQueryListener: () => void;
-    
-    @ViewChild(MatSidenav) snav: MatSidenav;
-    
+
     constructor(private productsService: ProductsService,
                 private route: ActivatedRoute,
-                private router: Router,
-                private changeDetectorRef: ChangeDetectorRef, 
-                private media: MediaMatcher) { 
-
-        this.mobileQuery = media.matchMedia('(max-width: 600px)');
-        this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-        this.mobileQuery.addListener(this._mobileQueryListener);
+                private router: Router) { 
     }
 
     ngOnInit() {
-        this.snav.open();
         this.fetchProducts();
 
         this.route.params.subscribe((params) => {
